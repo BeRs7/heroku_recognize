cae36e5d663ef2abceb5f84db02059223c0cfc62
BeRs <bers_99@mail.ru>
2022-07-02 03:44:26 +0600

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, UploadFile
+from pytesseract import pytesseract
+from PIL import Image
+app = FastAPI()
+
+pytesseract.tesseract_cmd = 'E:\\Tesseract\\tesseract.exe'
+
+
+@app.post("/pytesseract/")
+async def upload_pytesseract(file: UploadFile):
+    img = Image.open(file.file)
+    config = r'--oem 1 --psm 6'
+    ru = pytesseract.image_to_string(img, lang='rus', config=config)
+    eng = pytesseract.image_to_string(img, config=config)
+    return {
+        "RU": ru,
+        "ENG": eng
+    }
