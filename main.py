0b193ad630b7ccd24217d924ac7104418d9700e4
BeRs <bers_99@mail.ru>
2022-07-03 00:28:48 +0600

    change swagger url

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI, UploadFile
 from pytesseract import pytesseract
 from PIL import Image
-app = FastAPI()
+
+app = FastAPI(docs_url='/')
 
 pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
+config = r'--oem 3 --psm 6'
 
 
-@app.post("/pytesseract/")
-async def upload_pytesseract(file: UploadFile):
+@app.post("/rus/")
+async def upload_rus(file: UploadFile):
     img = Image.open(file.file)
-    config = r'--oem 3 --psm 6'
-    ru = pytesseract.image_to_string(img, lang='rus', config=config)
-    eng = pytesseract.image_to_string(img, config=config)
+    data = pytesseract.image_to_string(img, lang='rus', config=config)
     return {
-        "RU": ru,
-        "ENG": eng
+        "data": data
+    }
+
+
+@app.post("/eng/")
+async def upload_eng(file: UploadFile):
+    img = Image.open(file.file)
+    data = pytesseract.image_to_string(img, config=config)
+    return {
+        "data": data
     }
