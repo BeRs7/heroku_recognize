f447f0d5d7aefa33c8fb7a2854707c730bb4091b
BeRs <bers_99@mail.ru>
2022-07-02 16:16:27 +0600

    change config

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
 @app.post("/pytesseract/")
 async def upload_pytesseract(file: UploadFile):
     img = Image.open(file.file)
-    config = r'--oem 1 --psm 6'
+    config = r'--oem 3 --psm 6'
     ru = pytesseract.image_to_string(img, lang='rus', config=config)
     eng = pytesseract.image_to_string(img, config=config)
     return {
